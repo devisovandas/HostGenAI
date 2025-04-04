68821ac4521ecec037c874d53da3bcf4d0042a8f
DeviSovanDas <DeviSovanDas@WIN-OMFI987MUC4>
2025-04-04 17:44:31 +0530

    Adding Files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Dashboard from './Dashboard'; // Adjust the import path as necessary
+
+function App() {
+  return (
+    <Dashboard />
+  );
+}
+
+export default App;
